0e647fb0eab04042a979f50854b55a4f5ce89216
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-04-07 14:17:26 +0200

    Add button and config

--- frontend.py
+++ frontend.py
@@ -1,3 +1,9 @@
+"""
+# My first app with Streamlit
+
+This is a simple Streamlit app that demonstrates how to create a web application using Python.
+"""
+
 import streamlit as st
 
 # Set the page configuration
@@ -8,4 +14,21 @@ st.set_page_config(
 )
 
 # Set the title of the app
-st.title("My Streamlit page")
+st.title("My first Streamlit page")
+
+
+# Button
+def click_button():
+    print("Button clicked!")
+    st.toast("Button clicked!", icon="🎉")
+
+
+st.button(
+    "Click me!",
+    key="button1",
+    on_click=click_button,
+    help="Click this button to see a message",
+)
+
+if st.session_state.button1:
+    st.write("You clicked the button!")
